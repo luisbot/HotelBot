0aad53fc6f9929d3648e5665563a05ee65fe3783
nisar <nisar2048@gmail.com>
2018-01-22 00:46:23 +0530

    initial commit

---
+++ HotelBot/Dialogs/LUISDialog.cs
@@ -0,0 +1,83 @@
+﻿using HotelBot.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HotelBot.Dialogs
+{
+    [LuisModel("58504371-def9-43a0-a9f4-1380876f4806", "6d6eb70d52f54418b75dd386fdf5b900")]
+    [Serializable]
+    public class LUISDialog : LuisDialog<RoomReservation>
+    {
+        private readonly BuildFormDelegate<RoomReservation> ReserveRoom;
+
+        public LUISDialog(BuildFormDelegate<RoomReservation> reserveRoom)
+        {
+            this.ReserveRoom = reserveRoom;
+        }
+
+
+        [LuisIntent("")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("I'm sorry I don't know what you mean.");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("Greeting")]
+        public async Task Greeting(IDialogContext context, LuisResult result)
+        {
+            context.Call(new GreetingDialog(), Callback);
+        }
+
+        private async Task Callback(IDialogContext context, IAwaitable<object> result)
+        {
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("ReserveRoom")]
+        public async Task RoomReservation(IDialogContext context, LuisResult result)
+        {
+            var enrollmentForm = new FormDialog<RoomReservation>(new RoomReservation(), this.ReserveRoom, FormOptions.PromptInStart);
+            context.Call<RoomReservation>(enrollmentForm, Callback);
+        }
+
+        
+            [LuisIntent("Reservation")]
+        public async Task RoomReservation2(IDialogContext context, LuisResult result)
+        {
+            var enrollmentForm = new FormDialog<RoomReservation>(new RoomReservation(), this.ReserveRoom, FormOptions.PromptInStart);
+            context.Call<RoomReservation>(enrollmentForm, Callback);
+        }
+        [LuisIntent("QueryAmenity")]
+        public async Task QueryAmenities(IDialogContext context, LuisResult result)
+        {
+            foreach (var entity in result.Entities.Where(Entity => Entity.Type == "Amenity"))
+            {
+                var value = entity.Entity.ToLower();
+                if (value == "pool" || value == "gym" || value == "wifi" || value == "towels")
+                {
+                    await context.PostAsync("Yes we have that!");
+                    context.Wait(MessageReceived);
+                    return;
+                }
+                else
+                {
+                    await context.PostAsync("I'm sorry we don't have that.");
+                    context.Wait(MessageReceived);
+                    return;
+                }
+            }
+            await context.PostAsync("I'm sorry we don't have that.");
+            context.Wait(MessageReceived);
+            return;
+        }
+
+    }
+}
